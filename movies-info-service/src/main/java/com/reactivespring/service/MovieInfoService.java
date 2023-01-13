d0f30df2ae598bfb6d58483d081da03ef45b748a
Leonardo Batista Carias <leonardo1carias@gmail.com>
2023-01-13 17:54:00 -0300

    Section 14 - Build MovieInfo Service using RestController Approach

---
+++ movies-info-service/src/main/java/com/reactivespring/service/MovieInfoService.java
@@ -0,0 +1,44 @@
+package com.reactivespring.service;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MovieInfoService {
+
+    private MovieInfoRepository movieInfoRepository;
+
+    public MovieInfoService(MovieInfoRepository movieInfoRepository) {
+        this.movieInfoRepository = movieInfoRepository;
+    }
+
+    public Mono<MovieInfo> addMovieInfo(MovieInfo movieInfo) {
+        return movieInfoRepository.save(movieInfo);
+    }
+
+    public Flux<MovieInfo> getAllMovieInfos() {
+        return movieInfoRepository.findAll();
+    }
+
+    public Mono<MovieInfo> getMovieInfoById(String id) {
+        return movieInfoRepository.findById(id);
+    }
+
+    public Mono<MovieInfo> updateMovieInfo(MovieInfo updateMovieInfo, String id) {
+        return movieInfoRepository.findById(id)
+                .flatMap(movieInfo -> {
+                    movieInfo.setCast(updateMovieInfo.getCast());
+                    movieInfo.setName(updateMovieInfo.getName());
+                    movieInfo.setReleaseDate(updateMovieInfo.getReleaseDate());
+                    movieInfo.setYear(updateMovieInfo.getYear());
+                    return movieInfoRepository.save(movieInfo);
+                });
+    }
+
+    public Mono<Void> deleteMovieInfo(String id) {
+        return movieInfoRepository.deleteById(id);
+    }
+}
---
+++ movies-info-service/src/test/java/intg/com/reactivespring/controller/MoviesInfoControllerIntgTest.java
@@ -0,0 +1,136 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+@ImportAutoConfiguration(exclude = EmbeddedMongoAutoConfiguration.class)
+class MoviesInfoControllerIntgTest {
+
+    @Autowired
+    MovieInfoRepository repository;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    static String MOVIES_INFO_URL = "/v1/movieinfos";
+
+    @BeforeEach
+    void setUp() {
+        var movieInfos = List.of(new MovieInfo(null, "Batman Begins",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        2005, List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises",
+                        2005, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20")));
+        repository.saveAll(movieInfos)
+                .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll().block();
+    }
+
+    @Test
+    void addMovieInfo() {
+        final var movieInfo = new MovieInfo(null, "Batman Begins",
+                2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+        webTestClient
+                .post()
+                .uri(MOVIES_INFO_URL)
+                .bodyValue(movieInfo)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(MovieInfo.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+                    final var result = movieInfoEntityExchangeResult.getResponseBody();
+                    assert result != null;
+                    assert result.getMovieInfoId() != null;
+                });
+    }
+
+    @Test
+    void getAllMovieInfos() {
+        webTestClient
+                .get()
+                .uri(MOVIES_INFO_URL)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(MovieInfo.class)
+                .hasSize(3);
+    }
+
+    @Test
+    void getMovieInfoById() {
+        final var movieInfoId = "abc";
+        webTestClient
+                .get()
+                .uri(MOVIES_INFO_URL+"/{id}", movieInfoId)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.name").isEqualTo("Dark Knight Rises");
+
+//                .consumeWith(movieInfoEntityExchangeResult -> {
+//                    final var result = movieInfoEntityExchangeResult.getResponseBody();
+//                    assert result != null;
+//                    assert result.getMovieInfoId() != null;
+//                });
+    }
+
+    @Test
+    void updateMovieInfo() {
+        final var movieInfo = new MovieInfo("abc", "Batman Begins 2",
+                2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+        final var movieInfoId = "abc";
+
+        webTestClient
+                .put()
+                .uri(MOVIES_INFO_URL+"/{id}", movieInfoId)
+                .bodyValue(movieInfo)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(MovieInfo.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+                    final var result = movieInfoEntityExchangeResult.getResponseBody();
+                    assert result != null;
+                    assert result.getMovieInfoId() != null;
+                    assertEquals(result.getName(), "Batman Begins 2");
+                });
+    }
+
+    @Test
+    void deleteMovieInfo() {
+        final var movieInfoId = "abc";
+
+        webTestClient
+                .delete()
+                .uri(MOVIES_INFO_URL+"/{id}", movieInfoId)
+                .exchange()
+                .expectStatus()
+                .isNoContent();
+    }
+
+}
